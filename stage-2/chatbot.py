92df7b0fe570b27a61ff2c0dd591e72643d8ead2
ark-29 <abhiramavanapu29@gmail.com>
2026-08-18 21:52:35 +0530

    Add voice and text input

--- stage-2/chatbot.py
+++ stage-2/chatbot.py
@@ -2,15 +2,41 @@ from google import genai
 from dotenv import load_dotenv
 import os
 import time
+import speech_recognition as sr
+
+recognizer=sr.Recognizer()
 load_dotenv()
 
 gemini=genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 memory=[]
 max_tries=3
-while True:
 
-    user_input=input("Enter What You to know:  ")
+def audio_text():
+    with sr.Microphone() as source:
+        print("Listening....")
+        audio=recognizer.listen(source)
+
+    try:
+        text=recognizer.recognize_google(audio)
+        print("User: ",text)
+        return text
+    except Exception as e:
+        print("Error!!  ",e)
+        return None
+
+while True:
+    print("Choose input mode \n 1.Text \n 2.Audio")
+    choice=input("Enter your choice")
+    if choice=="1":
+        user_input=input("Enter What You to know:  ")
+    elif choice=="2":
+        user_input=audio_text()
+        if user_input is None:
+            continue
+    else:
+        print("Enter A valid choice.")
+        continue
 
     if user_input.lower() == "break" or user_input.lower()=="exit":
         print("Thank You!!")
@@ -35,13 +61,14 @@ while True:
             print()
 
             memory.append({
-                "role":"Model",
+                "role":"model",
                 "parts":[{"text":full_answer}]
             })
+            break
         except Exception as e:
             if i == max_tries-1:
                 print("\n request failed",e)
             else:
-                wait_time=2**max_tries
+                wait_time=2**i
                 print(f"\n Request Failed try again in {wait_time}")
                 time.sleep(wait_time)
